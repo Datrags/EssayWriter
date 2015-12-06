dc3cfe03438c1e80862c675760e485704ed48c5f
Datrags <stephan.green@scholar.democracyprep.org>
2015-12-06 18:04:46 +0000

    added some models and framework

--- application_controller.rb
+++ application_controller.rb
@@ -1,4 +1,5 @@
 require 'bundler'
+require "./models/model.rb"
 Bundler.require
 
 class MyApp < Sinatra::Base
@@ -6,12 +7,14 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
-  
+
   post '/essay' do
-    params[:length]
-    params[:topic]
-    params[:name]
-    
+    e = InitInfo.new(params[:name])
+    e.length = params[:length]
+    e.topic = params[:topic]
+    e.college = params[:college]
+
+
   end
-  
+
 end
